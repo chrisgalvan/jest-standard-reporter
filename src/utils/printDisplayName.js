dbbf4f0e4ab5affd4339cecccf7c20743c8e0381
Chris Galvan <chrisgalvanr@gmail.com>
2020-10-22 23:08:37 -0500

    feat: update file (merge conflicts)

--- src/utils/printDisplayName.js
+++ src/utils/printDisplayName.js
@@ -3,16 +3,20 @@ const chalk = require('chalk');
 const printDisplayName = (config) => {
   const { displayName } = config;
 
-  if (displayName) {
-    const nameToUse =
-      typeof displayName.name === 'string' ? displayName.name : displayName;
-
-    return chalk.supportsColor
-      ? chalk.reset.inverse.white(` ${nameToUse} `)
-      : nameToUse;
+  if (!displayName) {
+    return '';
   }
 
-  return '';
+  const name =
+    typeof displayName.name === 'string' ? displayName.name : displayName;
+  const chosenColor =
+    typeof displayName.color === 'string' ? displayName.color : 'white';
+
+  const print = chalk.reset.inverse[chosenColor]
+    ? chalk.reset.inverse[chosenColor]
+    : chalk.reset.inverse.white;
+
+  return chalk.supportsColor ? print(` ${name} `) : name;
 };
 
 module.exports = printDisplayName;
